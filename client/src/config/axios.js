b28d8333324be83e0f7c852e46c25ca62a148889
anshrusia200 <anshrusia2002@gmail.com>
2023-04-01 00:33:04 +0530

    [feat]: Import axios config to root

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://devcon-backend-c3zc.onrender.com/api"
-    : "http://localhost:5000/api";
+    ? "https://devcon-backend-c3zc.onrender.com"
+    : "http://localhost:5000";
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { MantineProvider } from '@mantine/core';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import "./config/axios";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <MantineProvider theme={{ colorScheme: 'dark' }}>
+  <MantineProvider theme={{ colorScheme: "dark" }}>
     <App />
   </MantineProvider>
 );
