8941a1d1c35f440f1be610a65f56d84c1d507288
anshrusia200 <anshrusia2002@gmail.com>
2023-03-31 23:33:42 +0530

    Backend deployed

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://devcon-backend-c3zc.onrender.com",
     },
   },
   build: {
